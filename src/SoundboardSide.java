cd396a272adc4ef541f2c452cdbb727261016e0c
seanpmaxwell <seanpmaxwell1@gmail.com>
2018-11-21 08:02:04 -0800

    started readme

--- src/SoundboardSide.java
+++ src/SoundboardSide.java
@@ -18,32 +18,32 @@ private static final String _TITLE_TXT = "Press 'Enter' to stop sound      PROJE
 
 SoundboardSide(SoundboardInner sboardInner)
 {
-	this._sboardInner = sboardInner;
+this._sboardInner = sboardInner;
 
-	// Set title
-	this._title = new JLabel(_TITLE_TXT  + "New Project");
-	this._title.setHorizontalAlignment(JLabel.CENTER);
+// Set title
+this._title = new JLabel(_TITLE_TXT  + "New Project");
+this._title.setHorizontalAlignment(JLabel.CENTER);
 
-	// Set scroll settings
-	var yScroll = JScrollPane.VERTICAL_SCROLLBAR_ALWAYS;
-	var xScroll = JScrollPane.HORIZONTAL_SCROLLBAR_NEVER;
-	var scroller = new JScrollPane(this._sboardInner, yScroll, xScroll);
+// Set scroll settings
+var yScroll = JScrollPane.VERTICAL_SCROLLBAR_ALWAYS;
+var xScroll = JScrollPane.HORIZONTAL_SCROLLBAR_NEVER;
+var scroller = new JScrollPane(this._sboardInner, yScroll, xScroll);
 
-	super.setBackground(Color.white);
-	super.setLayout(new BorderLayout());
-	super.add(this._title, BorderLayout.NORTH);
-	super.add(scroller, BorderLayout.CENTER);
+super.setBackground(Color.white);
+super.setLayout(new BorderLayout());
+super.add(this._title, BorderLayout.NORTH);
+super.add(scroller, BorderLayout.CENTER);
 }
 
 void setProjectTitle()
 {
-	super.remove(this._title);
+super.remove(this._title);
 
-	var prjTitle = this._sboardInner.getProjectTitle();
-	this._title.setText(_TITLE_TXT + prjTitle);
+var prjTitle = this._sboardInner.getProjectTitle();
+this._title.setText(_TITLE_TXT + prjTitle);
 
-	super.add(this._title, BorderLayout.NORTH);
-	super.repaint();
-	super.revalidate();
+super.add(this._title, BorderLayout.NORTH);
+super.repaint();
+super.revalidate();
 }
 }
