38392ff34d9e1f57ef331df8be021f9aa88fea68
seanpmaxwell <seanpmaxwell1@gmail.com>
2018-11-21 07:56:47 -0800

    started readme

--- src/SoundButton.java
+++ src/SoundButton.java
@@ -10,20 +10,20 @@ import javax.swing.JButton;
 
 public class SoundButton extends JButton implements Comparable<SoundButton>, Serializable
 {
-	private String _soundLabel;
-	private String _projectTitle;
+    private String _soundLabel;
+    private String _projectTitle;
     private String _track;
-	
-	SoundButton(String soundLabel)
+
+    SoundButton(String soundLabel)
     {
-		super(soundLabel);
-		this.setSoundLabel(soundLabel);
-	}
-	
-	String getSoundLabel()
-	{
-		return this._soundLabel;
-	}
+        super(soundLabel);
+        this.setSoundLabel(soundLabel);
+    }
+
+    String getSoundLabel()
+    {
+        return this._soundLabel;
+    }
 
     String getProjectTitle()
     {
@@ -34,40 +34,40 @@ public class SoundButton extends JButton implements Comparable<SoundButton>, Ser
     {
         return this._track;
     }
-	
-	void setSoundLabel(String soundLabel)
+
+    void setSoundLabel(String soundLabel)
     {
         this._soundLabel = soundLabel;
     }
 
-	void setProjectTitle(String projectTitle)
+    void setProjectTitle(String projectTitle)
     {
         this._projectTitle = projectTitle;
     }
 
     // track is a string representing a file path
-	void setTrack(String track)
+    void setTrack(String track)
     {
         this._track = track;
     }
 
-	public int compareTo(SoundButton otherBtn)
-	{
-		if(otherBtn == null || !(otherBtn instanceof SoundButton)) {
-		    throw new RuntimeException("Error Comparing Buttons");
+    public int compareTo(SoundButton otherBtn)
+    {
+        if(otherBtn == null || !(otherBtn instanceof SoundButton)) {
+            throw new RuntimeException("Error Comparing Buttons");
         }
 
         var otherLabel = otherBtn.getSoundLabel();
         int comparison = getSoundLabel().compareTo(otherLabel);
-				
-		if(comparison == 0) {
+
+        if(comparison == 0) {
             return 0;
         }
-		else if(comparison > 0) {
+        else if(comparison > 0) {
             return 1;
         }
-		else {
+        else {
             return -1;
         }
-	}
+    }
 }
